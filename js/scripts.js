12d1f55fbeb3be607e9da1ca2dc7748fc4a8f737
nestor <limnestor@consultant.com>
2020-04-03 19:15:19 +0800

    update service worker

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,14 @@ window.onload = function()
 {
 	if ('serviceWorker' in navigator) 
 	{
-		navigator.serviceWorker.register('js/service-worker.js');
+		navigator.serviceWorker.register('/js/service-worker.js')
+			.then(function(registration) 
+			{
+				console.log('Registration successful, scope is:', registration.scope);
+			})
+			.catch(function(error) 
+			{
+				console.log('Service worker registration failed, error:', error);
+			});
 	}
 }
